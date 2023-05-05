52305a946cce29fcaa58711f5502792f2e777ba0
JahanaSultan <cahana.aliyeva@gmail.com>
2023-05-05 18:50:14 +0400

    add praying time

--- README.md
+++ README.md
@@ -6,14 +6,14 @@
 
 - [Ərəb dilində Quran](https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions/ara-quranacademy.json)
 
+- [Surələr haqqında məlumtat](https://cdn.jsdelivr.net/gh/JahanaSultan/quran@latest/json/quran-chapter-info.json)
+
 - [Mishary Rashid Alafasy qiraət](https://alquran.cloud/api)
 
-- [Hicri təqvimi üçün](https://aladhan.com/islamic-calendar-api)
-
-- [Namaz vaxtları üçün](https://aladhan.com/prayer-times-api)
+- [Namaz vaxtları](https://aladhan.com/prayer-times-api)
 
 - [Logoda yer alan svg](https://iconscout.com/contributors/iconscout)
 
 - [Arxa plan şəkli](https://unsplash.com/photos/LanJEVFzxPw)
 
- 
+- [Geolocation](https://opencagedata.com/api)
--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ let hijri = document.querySelector('.hijri');
 let gregorian = document.querySelector('.gregorian');
 let clock = document.getElementById("clock")
 let loading_div = document.querySelector('.loading')
+let prayer_date = document.querySelector('.prayer-date')
 
 let days = [
     "Bazar ertəsi",
@@ -44,7 +45,7 @@ let hijri_months = [
     "Zilhicce"
 ];
 
-const loading=()=>{
+const loading = () => {
     loading_div.style.display = 'flex'
     setTimeout(() => {
         loading_div.style.display = 'none'
@@ -65,7 +66,7 @@ const loadPage = () => {
 
 
 const chapterNames = async () => {
-    fetch('https://cdn.jsdelivr.net/gh/JahanaSultan/quran/json/quran-chapter-info.json').
+    fetch('https://cdn.jsdelivr.net/gh/JahanaSultan/quran@latest/json/quran-chapter-info.json').
         then(res => res.json()).
         then(data => data.quran.map(chapter => chapters.innerHTML +=
             `<a onclick="navigatePage(this)" data-id=${chapter.chapter}>
@@ -106,11 +107,16 @@ const updateTime = () => {
 
 const today_date = async () => {
     let today = new Date()
-    gregorian.innerHTML += `<p>${today.getDate()} ${month_names[today.getMonth()]} ${today.getFullYear()}</p>`
-    let change_format = today.toLocaleDateString('en-GB').split('/').join('-');
-    fetch(`http://api.aladhan.com/v1/gToH/${change_format}`).
-        then(res => res.json()).
-        then(data => hijri.innerHTML += `<p>${data.data.hijri.day} ${hijri_months[data.data.hijri.month.number - 1]} ${data.data.hijri.year}</p>`)
+    let day = today.getDate()
+    let month = today.getMonth() + 1
+    let year = today.getFullYear()
+    if (localStorage.getItem('city') == null || localStorage.getItem('country') == null) {
+        await current_city()
+        prayer_times(localStorage.getItem('city'), localStorage.getItem('country'), year, month, day)
+    }
+    else {
+        prayer_times(localStorage.getItem('city'), localStorage.getItem('country'), year, month, day)
+    }
 }
 
 const daily_verse = async () => {
@@ -122,4 +128,43 @@ const daily_verse = async () => {
         }
         )
 }
+
+const current_city = async () => {
+    console.log("called")
+    navigator.geolocation.getCurrentPosition((position) => {
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+        fetch(`https://api.opencagedata.com/geocode/v1/json?q=${lat}+${long}&key=3e490cfc89ac4cce88823ab10ffd4c59`).
+            then(res => res.json()).
+            then(data => {
+                localStorage.setItem('city', data.results[0].components.city)
+                localStorage.setItem('country', data.results[0].components.country)
+            })
+    })
+}
+
+const prayer_times = async (city, country, year, month, day) => {
+    fetch(`https://api.aladhan.com/v1/calendarByCity/${year}/${month}?city=${city}&country=${country}&method=13`).
+        then(res => res.json()).
+        then(data => {
+            console.log(data.data)
+
+            prayer_date.innerHTML += `
+            <ul>
+                <li><span>Fəcr</span><span>${data.data[day-1].timings.Fajr.slice(0, 5)}</span></li>
+                <li><span>Günəş</span><span>${data.data[day-1].timings.Sunrise.slice(0, 5)}</span></li>
+                <li><span>Zöhr</span><span>${data.data[day-1].timings.Dhuhr.slice(0, 5)}</span></li>
+                <li><span>Əsr</span><span>${data.data[day-1].timings.Asr.slice(0, 5)}</span></li>
+                <li><span>Məğrib</span><span>${data.data[day-1].timings.Maghrib.slice(0, 5)}</span></li>
+                <li><span>İşa</span><span>${data.data[day-1].timings.Isha.slice(0, 5)}</span></li>
+            <ul>
+            `
+            gregorian.innerHTML += day + ' ' + month_names[month - 1] + ' ' + year
+            hijri.innerHTML += data.data[day-1].date.hijri.day + ' ' + hijri_months[data.data[day-1].date.hijri.month.number - 1] + ' ' + data.data[day-1].date.hijri.year
+        })
+
+}
+
+
+
 
